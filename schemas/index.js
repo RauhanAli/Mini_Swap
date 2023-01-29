a1b4bdf3ae6a0b602531cf85bfee855593f2ddae
rauhanali <rauhanali2@gmail.com>
2023-01-30 00:28:01 +0500

    Complete

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import { userSchema } from './userSchema'
+import { transactionSchema } from './transactionSchema'
+
+export const schemaTypes = [userSchema, transactionSchema]
+// export default createSchema({
+//   name: 'default',
+//   types: schemaTypes.concat([userSchema, transactionSchema]),
+// })
